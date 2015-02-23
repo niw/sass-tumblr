d7382643a0ea6d95efcfafe6259b69b63cf69950
Yoshimasa Niwa <niw@niw.at>
2015-02-22 16:45:34 -0800

    Rename variables, constants.

--- lib/sass_tumblr.rb
+++ lib/sass_tumblr.rb
@@ -4,29 +4,29 @@ module Sass
   module SCSS
     module RX
       # Pattern to match Tumblr tag.
-      TUMBLR_VAR = /({[a-zA-Z0-9_]+(?::[a-zA-Z0-9_]+)?})/
+      TUMBLR_TAG = /({[a-zA-Z0-9_]+(?::[a-zA-Z0-9_]+)?})/
     end
 
     # See lib/sass/scss/static_parser.rb
     class StaticParser
-      # Handle Tumblr variable tags in CSS selector
+      # Handle Tumblr tags in CSS selector
       # Used from CssParser directly, from StaticParser and Parser via RuleNode#try_to_parse_non_interpolated_rules.
       alias :qualified_name_without_tumblr :qualified_name
       def qualified_name(allow_star_name = false)
-        if var = tok(TUMBLR_VAR)
-          return nil, var
+        if tag = tok(TUMBLR_TAG)
+          return nil, tag
         else
           qualified_name_without_tumblr(allow_star_name)
         end
       end
 
-      # Handle Tumblr variable tags in CSS key
+      # Handle Tumblr tags in CSS key
       # Used from CssParser and StaticParser
       # This is needed even we've patched in Parser class since it's also overridden in StaticParser.
       alias :interp_ident_without_tumblr :interp_ident
       def interp_ident(ident = IDENT)
-        if var = tok(TUMBLR_VAR)
-          [var]
+        if tag = tok(TUMBLR_TAG)
+          [tag]
         else
           interp_ident_without_tumblr(ident)
         end
@@ -35,29 +35,29 @@ module Sass
 
     # See lib/sass/scss/parser.rb
     class Parser
-      # Handle Tumblr variable tags in CSS selector
+      # Handle Tumblr tags in CSS selector
       # Used from StaticParser and Parser.
       alias :almost_any_value_token_without_tumblr :almost_any_value_token
       def almost_any_value_token
-        tok(TUMBLR_VAR) || almost_any_value_token_without_tumblr
+        tok(TUMBLR_TAG) || almost_any_value_token_without_tumblr
       end
 
-      # Handle Tumblr variable tags in CSS key
+      # Handle Tumblr tags in CSS key
       # Used from StaticParser and Parser.
       alias :interp_ident_without_tumblr :interp_ident
       def interp_ident(ident = IDENT)
-        if var = tok(TUMBLR_VAR)
-          [var]
+        if tag = tok(TUMBLR_TAG)
+          [tag]
         else
           interp_ident_without_tumblr(ident)
         end
       end
 
-      # Avoid to parse first "{" as a block in declaration for Tumblr variable tags in CSS value
+      # Avoid to parse first "{" as a block in declaration for Tumblr tags in CSS value
       # Used from all parsers.
       alias :value_without_tumblr! :value!
       def value!
-        if var = tok?(TUMBLR_VAR)
+        if tok?(TUMBLR_TAG)
           sass_script(:parse)
         else
           value_without_tumblr!
@@ -69,12 +69,12 @@ module Sass
   # See lib/sass/script/lexer.rb
   module Script
     class Lexer
-      # Handle Tumblr variable tags in CSS value
+      # Handle Tumblr tags in CSS value
       # Parse as ident token (like `middle', `top') in the parser.
       # Used from all parsers.
       alias :ident_without_tumblr_var :ident
       def ident
-        if scan(TUMBLR_VAR)
+        if scan(TUMBLR_TAG)
           [:ident, @scanner[1]]
         else
           ident_without_tumblr_var
