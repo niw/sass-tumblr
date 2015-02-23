4af66e35c28559435e9eabb172efb4aa69e1f2ce
Yoshimasa Niwa <niw@niw.at>
2015-02-22 16:46:17 -0800

    Add tests for engine.

--- test/units/sass_tumblr_test.rb
+++ test/units/sass_tumblr_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../test_helper", __FILE__)
 
 class SassTumblrTest < Test::Unit::TestCase
-  {
+  TestCases = {
     :tag_in_selector => [
       "{Tag:Tag} { key: value; }",
       "{Tag:Tag}{key:value}"
@@ -14,13 +14,17 @@ class SassTumblrTest < Test::Unit::TestCase
       "selector { key: {Tag} ident {Tag:Tag}; }",
       "selector{key:{Tag} ident {Tag:Tag}}"
     ]
-  }.each do |test_name, (css, expected)|
-    [
-      Sass::SCSS::Parser,
-      Sass::SCSS::StaticParser,
-      Sass::SCSS::CssParser
-    ].each do |parser_class|
-      define_method :"test_#{parser_class.name}_with_#{test_name}" do
+  }
+
+  ScssParsers = {
+    "parser" => Sass::SCSS::Parser,
+    "static_parser" => Sass::SCSS::StaticParser,
+    "css_parser" => Sass::SCSS::CssParser
+  }
+
+  TestCases.each do |test_name, (css, expected)|
+      ScssParsers.each do |parser_name, parser_class|
+      define_method :"test_#{parser_name}_with_#{test_name}" do
         parser = parser_class.new(css, "test", 1)
 
         node = nil
@@ -30,6 +34,24 @@ class SassTumblrTest < Test::Unit::TestCase
 
         node.options = {:style => :compressed}
         assert_equal expected, node.render.strip
+      end
+    end
+  end
+
+  [
+    #:sass, # NOTE: Old indent format is not supported yet.
+    :scss
+  ].each do |syntax|
+    TestCases.each do |test_name, (css, expected)|
+      define_method :"test_#{syntax}_engine_with_#{test_name}" do
+        engine = Sass::Engine.new(css, :syntax => syntax, :style => :compressed)
+
+        result = nil
+        assert_nothing_raised do
+          result = engine.render
+        end
+
+        assert_equal expected, result.strip
       end
     end
   end
